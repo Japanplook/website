abc72c21cf302c56b7267653816c8060a4ae251c
JapanLivibe <japan@livibe.co>
2026-05-17 21:35:24 +0700

    Fix: Favicon

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://plook.co";
+export const dynamic = "force-static";
+
+const BASE_URL = "https://plook.ag";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
